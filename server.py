8f41a482ccb477428fcff3519fe6fee3887e8346
Aksgo <akshitgoel0412@gmail.com>
2024-12-24 10:53:34 +0530

    static path updated

--- server.py
+++
@@ -1,8 +0,0 @@
-# from flask import Flask, render_template
-# app = Flask(__name__, template_folder="docs")
-# @app.route('/')
-# def home():
-#     return render_template('admin.html')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', debug=True)
